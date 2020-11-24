23f82c8fce4f7f35cdee119412af3ea7f9e34388
astudillome <projectagility@gmail.com>
2020-11-24 14:48:58 -0700

    adds functionality to dashboard.ejs, creates routes

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,8 +3,10 @@ const passport = require('../config/ppConfig');
 const db = require('../models');
 const router = express.Router();
 
-router.get('/dashboard', (req, res) => {
+router.get('/', (req, res) => {
   db.user.findAll().then((usersFound) => {
-    res.render('/dashboard', {users: usersFound})
+    res.render('dashboard', {users: usersFound})
   })
 } )
+
+module.exports = router;
